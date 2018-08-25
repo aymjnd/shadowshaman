f8e8efb8d61ef15c48fe020f79dc4dce4f5faa08
Anaz Haidhar <mr.anazhd@gmail.com>
2018-08-25 20:18:05 +0800

    better

--- shadowshaman.sh
+++ shadowshaman.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 sudo apt install curl -y
 clear
-netip=$(curl icanhazip.com)
-echo -e $"\e[32mWould you like to use your local ip?\n1) $netip (recommended)\n2) Local IP (e.g 192.168.0.1)"
+ip=$(curl icanhazip.com)
+echo -e $"\e[32mWhich IP?\n1) $ip (recommended)\n2) Local IP (e.g 192.168.0.1)"
 read local
 
 if [ "$local" == "1" ] ; then
     ip=$(curl icanhazip.com)
-
 elif [ "$local" == "2" ] ; then
 	echo -e $"\e[32mWhat is your Local IP?\e[39m"
 	read ip
 else
     echo -e $"\e[31mWrong input! Exiting.\e[39m"
+    exit 1
 fi
 
 echo -e $"\e[32mEnter your port. Default \e[31m443\e[39m (enter)"
@@ -22,18 +22,39 @@ if [ "$port" == "" ] ; then
 elif [ "$port" -gt "65535" ] ; then
     echo -e $"\e[31mPort can't be larger than 65535! Exiting\e[39m"
     exit 1
-elif [ "$port" == "2" ] ; then
+elif [ "$port" == "22" ] ; then
     echo -e $"\e[31mCan't use SSH port! Exiting\e[39m"
     exit 1
 fi
+
+echo -e $"\e[32mWhich encryption?\n1) chacha20-ietf-poly1305 (fastest)\n2) chacha20-ietf\n3) chacha20\n4) salsa20\n5) aes-256-cfb (standard)\n6) aes-256-gcm\n7) aes-256-ctr"
+read mtd
+if [ "$local" == "1" ] ; then
+    method=chacha20-ietf-poly1305
+elif [ "$local" == "2" ] ; then
+	method=chacha20-ietf
+elif [ "$local" == "3" ] ; then
+	method=chacha20
+elif [ "$local" == "4" ] ; then
+	method=salsa20
+elif [ "$local" == "5" ] ; then
+	method=aes-256-cfb
+elif [ "$local" == "6" ] ; then
+	method=aes-256-gcm
+elif [ "$local" == "7" ] ; then
+	method=aes-256-ctr
+else
+    echo -e $"\e[31mWrong input! Exiting.\e[39m"
+    exit 1
+fi
+
 echo -e $"\e[32mYour password?\e[39m"
 read password
-method=aes-256-cfb
 
 sudo apt update
-sudo apt install -y python python-pip
+sudo apt install -y python python-pip python-m2crypto libsodium18
 sudo python -m pip install --upgrade pip
-sudo pip install shadowsocks
+sudo pip install --upgrade https://github.com/shadowsocks/shadowsocks/archive/master.zip
 
 cd /bin
 SS="shadowsocks-start"
@@ -54,14 +75,14 @@ sudo chmod 755 shadowsocks-start
 sudo chmod 755 shadowsocks-stop
 
 cd
-grep -q -F 'nohup /bin/shadowsocks-start >> /dev/null &' /etc/rc.local || echo 'nohup /bin/shadowsocks-start >> /dev/null &' >> /etc/rc.local
+grep -q -F '/bin/shadowsocks-start >> /dev/null &' /etc/rc.local || sed -i "`wc -l < /etc/rc.local`i\\/bin/shadowsocks-start >> /dev/null &\\" /etc/rc.local
 shadowsocks-start
 
 url=$(echo "$method:$password@$ip:$port" | base64)
 b64url="ss://$url"
 
 clear
-echo -e "===================Shadowshaman=0.1b==================="
+echo -e "===================Shadowshaman=0.1c==================="
 echo -e "\nShadowsocks started! \e[32m$ip:$port\e[39m."
 echo -e "\nShadowsocks uri:\n\e[32m$b64url#shadowshaman\e[39m"
 echo -e "\nRun \e[31mshadowsocks-stop \e[39mto stop.\nShadowsocks installed as service and will auto restart on reboot\n"
